2f11248019f0c570fef88535ee1fe37ec8423a14
dekokun <shintaro.kurachi@gmail.com>
2012-10-13 01:49:38 +0900

    pi追加

--- RPN.hs
+++ RPN.hs
@@ -7,4 +7,5 @@ solveRPN = head . foldl foldingFunction [] . words
         foldingFunction (x:y:ys) "^" = (y ** x):ys
         foldingFunction (x:xs) "ln" = log x:xs
         foldingFunction xs "sum" = [sum xs]
+        foldingFunction xs "pi" = pi:xs
         foldingFunction xs numberString = read numberString:xs
