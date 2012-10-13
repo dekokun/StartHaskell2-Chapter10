5be3e557089132301a46df7727ea50ee3392b952
dekokun <shintaro.kurachi@gmail.com>
2012-10-13 16:11:02 +0900

    コメントアウトにしていた部分のコメントアウトを外した

--- toLondon.hs
+++ toLondon.hs
@@ -7,56 +7,56 @@ data Section = Section { getA :: Int, getB :: Int, getC :: Int }
 
 type RoadSystem = [Section]
 
--- heathrowToLondon :: RoadSystem
--- heathrowToLondon = [ Section 50 10 30
---                     , Section 5 90 30
---                     , Section 40 2 25
---                     , Section 10 8 0
---                     ]
+heathrowToLondon :: RoadSystem
+heathrowToLondon = [ Section 50 10 30
+                    , Section 5 90 0
+                    , Section 40 2 25
+                    , Section 10 8 0
+                    ]
 -- 
--- data Label = A | B | C deriving (Show)
--- type Path = [(Label, Int)]
+data Label = A | B | C deriving (Show)
+type Path = [(Label, Int)]
 -- 
--- roadStep :: (Path, Path) -> Section -> (Path, Path)
--- roadStep (pathA, pathB) (Section a b c) =
---     let timeA = sum (map snd pathA)
---         timeB = sum (map snd pathB)
---         forwardTimeToA = timeA + a
---         crossTimeToA = timeB + b + c
---         forwardTimeToB = timeB + b
---         crossTimeToB = timeA + a + c
---         newPathToA = if forwardTimeToA <= crossTimeToA
---                         then (A, a):pathA
---                         -- 直感的にpathA ++ (A, a)にしたいところだが…
---                         -- xs ++ ysは、xsを分解して全部(:)でつなげるということをするから遅いんですよね…
---                         -- P138参照
---                         else (C, c):(B, b):pathB
---         newPathToB = if forwardTimeToB <= crossTimeToB
---                         then (B, b):pathB
---                         else (C, c):(A, a):pathA
---     in (newPathToA, newPathToB)
+roadStep :: (Path, Path) -> Section -> (Path, Path)
+roadStep (pathA, pathB) (Section a b c) =
+    let timeA = sum (map snd pathA)
+        timeB = sum (map snd pathB)
+        forwardTimeToA = timeA + a
+        crossTimeToA = timeB + b + c
+        forwardTimeToB = timeB + b
+        crossTimeToB = timeA + a + c
+        newPathToA = if forwardTimeToA <= crossTimeToA
+                        then (A, a):pathA
+                        -- 直感的にpathA ++ (A, a)にしたいところだが…
+                        -- xs ++ ysは、xsを分解して全部(:)でつなげるということをするから遅いんですよね…
+                        -- P138参照
+                        else (C, c):(B, b):pathB
+        newPathToB = if forwardTimeToB <= crossTimeToB
+                        then (B, b):pathB
+                        else (C, c):(A, a):pathA
+    in (newPathToA, newPathToB)
 -- 
--- optimalPath :: RoadSystem -> Path
--- optimalPath roadSystem =
---     let (bestAPath, bestBPath) = foldl roadStep ([], []) roadSystem
---     in  if sum (map snd bestAPath) <= sum (map snd bestBPath)
---             then reverse bestAPath
---             else reverse bestBPath
+optimalPath :: RoadSystem -> Path
+optimalPath roadSystem =
+    let (bestAPath, bestBPath) = foldl roadStep ([], []) roadSystem
+    in  if sum (map snd bestAPath) <= sum (map snd bestBPath)
+            then reverse bestAPath
+            else reverse bestBPath
 -- 
 -- 
--- groupsOf :: Int -> [a] -> [[a]]
--- groupsOf 0 _ = undefined
--- groupsOf _ [] = []
--- groupsOf n xs = take n xs : groupsOf n (drop n xs)
+groupsOf :: Int -> [a] -> [[a]]
+groupsOf 0 _ = undefined
+groupsOf _ [] = []
+groupsOf n xs = take n xs : groupsOf n (drop n xs)
 -- 
--- main :: IO ()
--- main = do
---     contents <- getContents
---     let threes = groupsOf 3 (map read $ lines contents)
---         roadSystem = map (\[a,b,c] -> Section a b c) threes
---         path = optimalPath roadSystem
---         -- concat $ map f xsはconcatMap f xsにまとめられます
---         pathString = concatMap (show . fst) path
---         pathTime = sum $ map snd path
---     putStrLn $ "The best path to take is: " ++ pathString
---     putStrLn $ "Time taken: " ++ show pathTime
+main :: IO ()
+main = do
+    contents <- getContents
+    let threes = groupsOf 3 (map read $ lines contents)
+        roadSystem = map (\[a,b,c] -> Section a b c) threes
+        path = optimalPath roadSystem
+        -- concat $ map f xsはconcatMap f xsにまとめられます
+        pathString = concatMap (show . fst) path
+        pathTime = sum $ map snd path
+    putStrLn $ "The best path to take is: " ++ pathString
+    putStrLn $ "Time taken: " ++ show pathTime
